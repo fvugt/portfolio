88e37d853fbb922e4ffe5a244ad8d03f78f8a86a
fvugt <60261045+fvugt@users.noreply.github.com>
2025-12-01 13:50:59 +0100

    Github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Vul dit aan met de NAAM van jouw GitHub repository!
+const basePath = '/portfolio/';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: basePath,
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        '404': 'index.html',
+      },
+    },
+  },
+});
